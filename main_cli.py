7da9b395abfc5c2911fdf87575e62f959f539585
sj-v-808 <sachwlangjamatiago@gmail.com>
2020-10-26 17:19:33 +0530

    1st commit

--- main_cli.py
+++ main_cli.py
@@ -1,2 +1,10 @@
-print("helllo")
-print("hello again")
+from pytube import YouTube
+
+try:
+    yt = YouTube("https://www.youtube.com/watch?v=9lq0rL0CtYE&list=RD9lq0rL0CtYE&start_radio=1")#.streams[0].download()
+    print(yt.title)
+    print(yt.thumbnail_url)
+#use this
+#pip3 install --user PyTube3
+except:
+    print("error bitch")
